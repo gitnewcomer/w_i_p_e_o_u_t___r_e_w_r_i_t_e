57759e77ff6766c1802f4578d433f50f4e736be5
gitnewcomer <michaelfsantos@gmail.com>
2023-08-17 15:48:15 +0200

    Delete src directory

--- src/wipeout/main_menu.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_MENU_H
-#define MAIN_MENU_H
-
-void main_menu_init();
-void main_menu_update();
-
-#endif
--- src/wipeout/title.h
+++
@@ -1,8 +0,0 @@
-#ifndef TITLE_H
-#define TITLE_H
-
-void title_init();
-void title_update();
-void title_cleanup();
-
-#endif
